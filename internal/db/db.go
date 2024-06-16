5e8d25fad414e973f3b24900413ad75ef96208e6
Simon K <jackofspaces@gmail.com>
2024-06-16 12:57:32 +0100

    add `db` dependencies; wire up basic pgres container connectivity

--- internal/db/db.go
+++ internal/db/db.go
@@ -1,18 +1,25 @@
 package db
 
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Queryer interface {
+}
+
+var _ Queryer = (*Client)(nil)
+
 var client *Client
 
 type Client struct {
+	*sqlx.DB
 }
 
-func Init(connectionString string) {
-	client = New(connectionString)
-
-}
-
-func New(connectionString string) *Client {
-	return &Client{}
-
+func Init(connectionString string) error {
+	db := sqlx.MustConnect("postgres", connectionString)
+	client = &Client{DB: db}
+	return nil
 }
 
 func GetDB() *Client {
