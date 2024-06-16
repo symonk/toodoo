75086edae95eb472e457d0a83f41c29265b93124
Simon K <jackofspaces@gmail.com>
2024-06-16 17:18:47 +0100

    fix queries; add a `/api/v1/tasks/` endpoint

--- internal/controller/task.go
+++ internal/controller/task.go
@@ -2,11 +2,13 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/symonk/toodoo/internal/db"
+	"github.com/symonk/toodoo/internal/model"
 )
 
 type TaskHandler struct {
 }
+
+var taskModel = new(model.TaskModel)
 
 // @BasePage /api/v1
 // Retrieve godoc
@@ -18,9 +20,12 @@ type TaskHandler struct {
 // @Success 200 {object} model.Task
 // @Router /tasks/ [get]
 func (t TaskHandler) View(c *gin.Context) {
-	db := db.GetDB()
-	_ = db
-	task := make(map[string]string)
-	task["first task"] = "A first task"
-	c.JSON(200, task)
+	tasks, err := taskModel.RetrieveTasks(c.Request.Context())
+	if err != nil {
+		// TODO: standardize and improve errors in general etc.
+		// Do not propagate internal errors back to clients;
+		// Doing this for development only for now;
+		c.AbortWithStatusJSON(500, gin.H{"error": err.Error()})
+	}
+	c.JSON(200, tasks)
 }
--- internal/model/task.go
+++ internal/model/task.go
@@ -1,19 +1,41 @@
 package model
 
-import "time"
+import (
+	"context"
+	"time"
 
-type Task struct {
-	name        string
-	description string
-	recurring   bool
-	schedule    time.Time
+	"github.com/symonk/toodoo/internal/db"
+)
+
+const (
+	fetchAllTasks = "SELECT * FROM task;"
+	fetchTask     = "SELECT * FROM task WHERE id = ? LIMIT 1"
+)
+
+// TaskModel is the core encapsulation of a task
+type TaskModel struct {
+	Id          int       `json:"-"`
+	Name        string    `json:"name"`
+	Description string    `json:"description"`
+	Recurring   bool      `json:"recurring"`
+	Schedule    time.Time `json:"schedule"`
 }
 
-func NewTask() *Task {
-	return &Task{
-		name:        "foo",
-		description: "bar",
-		recurring:   true,
-		schedule:    time.Now().Add(time.Minute),
+func NewTask() *TaskModel {
+	return &TaskModel{
+		Name:        "foo",
+		Description: "bar",
+		Recurring:   true,
+		Schedule:    time.Now().Add(time.Minute),
 	}
+}
+
+// RetrieveTasks fetches all tasks from the database.
+func (t TaskModel) RetrieveTasks(ctx context.Context) ([]TaskModel, error) {
+	tasks := make([]TaskModel, 0)
+	db := db.GetDB()
+	if err := db.SelectContext(ctx, &tasks, fetchAllTasks); err != nil {
+		return tasks, err
+	}
+	return tasks, nil
 }
