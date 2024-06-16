0ac8d16636e8e9ffb24aa506932dbd572e22e522
Simon K <jackofspaces@gmail.com>
2024-06-16 12:40:25 +0100

    fix bootstrapping sql; dummy task and add `task` handler

--- sql/bootstrap.sql
+++ sql/bootstrap.sql
@@ -4,12 +4,12 @@ CREATE TABLE IF NOT EXISTS task (
     id SERIAL PRIMARY KEY,
     name varchar(250) NOT NULL,
     description varchar(250) NOT NULL,
-    recurring boolean() NOT NULL,
-    schedule timestamp,
+    recurring boolean NOT NULL,
+    schedule timestamp without time zone NOT NULL DEFAULT(current_timestamp AT TIME ZONE 'UTC')
 );
 
 -- Create some simply dummy data
 INSERT INTO task
     (name, description, recurring, schedule)
     VALUES
-    ("foo", "bar", false, 1999-01-08)
+    ('foo', 'bar', false, '2024-01-01')
