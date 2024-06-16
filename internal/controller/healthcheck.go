35cd1f611bbfa66a4a5679a1ef9510b8a5b668be
Simon K <jackofspaces@gmail.com>
2024-06-16 11:12:13 +0100

    adding new `scripts` dir; basics of a `healthcheck` endpoint and placeholder `V1` support

--- internal/controller/healthcheck.go
+++ internal/controller/healthcheck.go
@@ -1,1 +1,25 @@
 package controller
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+var started = time.Now()
+
+type Handler struct{}
+
+type HealthCheckHandler struct {
+}
+
+func (h HealthCheckHandler) Status(c *gin.Context) {
+	since := time.Since(started)
+	c.JSON(200, gin.H{"Uptime": since.String()})
+}
+
+// since returns the total uptime for the server in a
+// string format.
+func (h HealthCheckHandler) Since() string {
+	return time.Since(started).String()
+}
--- internal/server/router.go
+++ internal/server/router.go
@@ -1,8 +1,20 @@
 package server
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/symonk/toodoo/internal/controller"
+)
 
 func newRouter() *gin.Engine {
 	router := gin.Default()
+
+	// Register default (unprotected, non versioned) routes
+	healthCheckHandler := controller.HealthCheckHandler{}
+	router.GET("/healthcheck", healthCheckHandler.Status)
+
+	// Manage API versioned routes
+	apiV1 := router.Group("/v1")
+	apiV1.GET("/placeholder", func(c *gin.Context) {})
+
 	return router
 }
