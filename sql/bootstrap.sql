a00156164b3f2f620e8f614a0f4bdb12caa1c379
Simon K <jackofspaces@gmail.com>
2024-07-12 11:17:55 +0100

    adding `users` table

--- sql/bootstrap.sql
+++ sql/bootstrap.sql
@@ -1,5 +1,15 @@
--- TODO: Actually build this out properly after docker.
--- Creation of an task table
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    -- will want an email in future
+    username varchar(25) NOT NULL,
+    password varchar(100) NOT NULL,
+    admin boolean NOT NULL
+);
+
+INSERT INTO users (username, password, admin) VALUES ('admin', 'admin', true), ('readonly', 'readonly', false);
+
+
+-- Creation of a task table
 CREATE TABLE IF NOT EXISTS task (
     id SERIAL PRIMARY KEY,
     name varchar(250) NOT NULL,
