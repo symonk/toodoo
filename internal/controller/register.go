7a64f30ae603c3b502c2edcf29709c465cf4a21f
Simon K <jackofspaces@gmail.com>
2024-07-12 11:52:20 +0100

    add docs for `/api/v1/register`

--- internal/controller/register.go
+++ internal/controller/register.go
@@ -1,1 +1,19 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+type RegisterHandler struct {
+}
+
+// @BasePage /api/v1
+// Retrieve godoc
+// @Registers a new user to the system
+// @Schemes
+// @Description Registers a new user to the system
+// @Tags register
+// @Produce json
+// @Success 201 {object} []model.TaskModel
+// @Router /task [post]
+func (r RegisterHandler) Register(c *gin.Context) {
+
+}
